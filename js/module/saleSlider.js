c8fa8657653ce4a99644b6dbf0dfab8785e0a832
kkirrin <khlebnikov-2016@mail.ru>
2024-02-01 17:48:46 +1000

    slider починил

--- js/module/saleSlider.js
+++ js/module/saleSlider.js
@@ -7,9 +7,6 @@ export const initSaleSlider = () => {
             slidesPerView: 2,
             spaceBetween: 15,
             speed: 1200,
-            grid: {
-                rows: 2,
-            },
             navigation: {
                 nextEl: '.next-new--item-1',
                 prevEl: '.prev-new--item-1',
@@ -17,23 +14,14 @@ export const initSaleSlider = () => {
 
             breakpoints: {
                 320: {
-                    grid: {
-                        rows: 2,
-                    },
                 },
                 460: {
-                    grid: {
-                        rows: 1,
-                    },
                     slidesPerView: 1.5,
                     spaceBetween: 15
                 },
                 767: {
                     slidesPerView: 3,
                     spaceBetween: 15,
-                    grid: {
-                        rows: 1,
-                    },
                 }
             }
         });
